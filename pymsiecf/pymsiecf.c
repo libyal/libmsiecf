47de7ccff457a49de743a95e4b4a5fbe2e4e46e5
Joachim Metz <joachim.metz@gmail.com>
2014-10-04 19:22:43 +0200

    Worked on Python bindings.

--- pymsiecf/pymsiecf.c
+++ pymsiecf/pymsiecf.c
@@ -126,8 +126,6 @@ PyObject *pymsiecf_get_version(
 	         errors ) );
 }
 
-#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
-
 /* Checks if the file has a MSIE Cache File (index.dat) file signature
  * Returns a Python object if successful or NULL on error
  */
@@ -144,17 +142,22 @@ PyObject *pymsiecf_check_file_signature(
 	libcerror_error_t *error      = NULL;
 	static char *function         = "pymsiecf_check_file_signature";
 	static char *keyword_list[]   = { "filename", NULL };
-	const wchar_t *filename_wide  = NULL;
 	const char *filename_narrow   = NULL;
 	char *error_string            = NULL;
 	int result                    = 0;
+
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	const wchar_t *filename_wide  = NULL;
+#else
+	PyObject *utf8_string_object  = NULL;
+#endif
 
 	PYMSIECF_UNREFERENCED_PARAMETER( self )
 
 	/* Note that PyArg_ParseTupleAndKeywords with "s" will force Unicode strings to be converted to narrow character string.
 	 * On Windows the narrow character strings contains an extended ASCII string with a codepage. Hence we get a conversion
-	 * exception. We cannot use "u" here either since that does not allow us to pass non Unicode string objects and
-	 * Python (at least 2.7) does not seems to automatically upcast them.
+	 * exception. This will also fail if the default encoding is not set correctly. We cannot use "u" here either since that
+	 * does not allow us to pass non Unicode string objects and Python (at least 2.7) does not seems to automatically upcast them.
 	 */
 	if( PyArg_ParseTupleAndKeywords(
 	     arguments,
@@ -208,6 +211,7 @@ PyObject *pymsiecf_check_file_signature(
 	{
 		PyErr_Clear();
 
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
 		filename_wide = (wchar_t *) PyUnicode_AsUnicode(
 		                             string_object );
 		Py_BEGIN_ALLOW_THREADS
@@ -217,7 +221,57 @@ PyObject *pymsiecf_check_file_signature(
 		          &error );
 
 		Py_END_ALLOW_THREADS
+#else
+		utf8_string_object = PyUnicode_AsUTF8String(
+		                      string_object );
 
+		if( utf8_string_object == NULL )
+		{
+			PyErr_Fetch(
+			 &exception_type,
+			 &exception_value,
+			 &exception_traceback );
+
+			exception_string = PyObject_Repr(
+					    exception_value );
+
+			error_string = PyString_AsString(
+					exception_string );
+
+			if( error_string != NULL )
+			{
+				PyErr_Format(
+				 PyExc_RuntimeError,
+				 "%s: unable to convert unicode string to UTF-8 with error: %s.",
+				 function,
+				 error_string );
+			}
+			else
+			{
+				PyErr_Format(
+				 PyExc_RuntimeError,
+				 "%s: unable to convert unicode string to UTF-8.",
+				 function );
+			}
+			Py_DecRef(
+			 exception_string );
+
+			return( NULL );
+		}
+		filename_narrow = PyString_AsString(
+				   utf8_string_object );
+
+		Py_BEGIN_ALLOW_THREADS
+
+		result = libmsiecf_check_file_signature(
+		          filename_narrow,
+		          &error );
+
+		Py_END_ALLOW_THREADS
+
+		Py_DecRef(
+		 utf8_string_object );
+#endif
 		if( result == -1 )
 		{
 			pymsiecf_error_raise(
@@ -324,74 +378,11 @@ PyObject *pymsiecf_check_file_signature(
 	}
 	PyErr_Format(
 	 PyExc_TypeError,
-	 "%s: unsupported string object type",
+	 "%s: unsupported string object type.",
 	 function );
 
 	return( NULL );
 }
-
-#else
-
-/* Checks if the file has a MSIE Cache File (index.dat) file signature
- * Returns a Python object if successful or NULL on error
- */
-PyObject *pymsiecf_check_file_signature(
-           PyObject *self PYMSIECF_ATTRIBUTE_UNUSED,
-           PyObject *arguments,
-           PyObject *keywords )
-{
-	libcerror_error_t *error    = NULL;
-	static char *function       = "pymsiecf_check_file_signature";
-	static char *keyword_list[] = { "filename", NULL };
-	const char *filename        = NULL;
-	int result                  = 0;
-
-	PYMSIECF_UNREFERENCED_PARAMETER( self )
-
-	if( PyArg_ParseTupleAndKeywords(
-	     arguments,
-	     keywords,
-	     "|s",
-	     keyword_list,
-	     &filename ) == 0 )
-	{
-		return( NULL );
-	}
-	Py_BEGIN_ALLOW_THREADS
-
-	result = libmsiecf_check_file_signature(
-	          filename,
-	          &error );
-
-	Py_END_ALLOW_THREADS
-
-	if( result == -1 )
-	{
-		pymsiecf_error_raise(
-		 error,
-		 PyExc_IOError,
-		 "%s: unable to check file signature.",
-		 function );
-
-		libcerror_error_free(
-		 &error );
-
-		return( NULL );
-	}
-	if( result != 0 )
-	{
-		Py_IncRef(
-		 (PyObject *) Py_True );
-
-		return( Py_True );
-	}
-	Py_IncRef(
-	 (PyObject *) Py_False );
-
-	return( Py_False );
-}
-
-#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
 
 /* Checks if the file has a MSIE Cache File (index.dat) file signature using a file-like object
  * Returns a Python object if successful or NULL on error
