8fa97a23e5b82a86974ea887819507c36a7a6f06
Joachim Metz <joachim.metz@gmail.com>
2014-11-18 07:55:30 +0100

    Code clean up.

--- libmsiecf/libmsiecf_file.h
+++ libmsiecf/libmsiecf_file.h
@@ -95,13 +95,15 @@ int libmsiecf_file_open(
      libcerror_error_t **error );
 
 #if defined( HAVE_WIDE_CHARACTER_TYPE )
+
 LIBMSIECF_EXTERN \
 int libmsiecf_file_open_wide(
      libmsiecf_file_t *file,
      const wchar_t *filename,
      int access_flags,
      libcerror_error_t **error );
-#endif
+
+#endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
 LIBMSIECF_EXTERN \
 int libmsiecf_file_open_file_io_handle(
