f952ab16547a4a416104135e10747141b54ac00c
Joachim Metz <joachim.metz@gmail.com>
2016-01-27 07:52:41 +0100

    Worked on tests.

--- tests/msiecf_test_open_close.c
+++ tests/msiecf_test_open_close.c
@@ -20,12 +20,11 @@
  */
 
 #include <common.h>
+#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
 #endif
-
-#include <stdio.h>
 
 #include "msiecf_test_libcerror.h"
 #include "msiecf_test_libcstring.h"
