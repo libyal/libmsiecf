3f8aaf7f732e3f9d366e6a45488d85c8a489638a
Joachim Metz <joachim.metz@gmail.com>
2021-01-12 06:55:46 +0100

    Applied updates

--- libmsiecf/libmsiecf.c
+++ libmsiecf/libmsiecf.c
@@ -1,7 +1,7 @@
 /*
  * Library to access the MSIE Cache File (index.dat) format
  *
- * Copyright (C) 2009-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- msiecftools/msiecfinfo.c
+++ msiecftools/msiecfinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a MSIE Cache File (index.dat)
  *
- * Copyright (C) 2009-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
