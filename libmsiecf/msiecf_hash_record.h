4f0ab05dec00657242b7d93f6074529829502832
Joachim Metz <joachim.metz@gmail.com>
2022-02-11 06:21:25 +0100

    Applied updates

--- libmsiecf/msiecf_hash_record.h
+++ libmsiecf/msiecf_hash_record.h
@@ -1,7 +1,7 @@
 /*
  * The HASH record definition of a MSIE Cache File
  *
- * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
