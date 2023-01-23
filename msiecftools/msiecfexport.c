ba9f9a07b63852997154a7a839454f25a47f61e0
Joachim Metz <joachim.metz@gmail.com>
2023-01-23 05:07:19 +0100

    Applied updates

--- msiecftools/msiecfexport.c
+++ msiecftools/msiecfexport.c
@@ -1,7 +1,7 @@
 /*
  * Extracts items from from a MSIE Cache File (index.dat)
  *
- * Copyright (C) 2009-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
