cb52772cd66cac9efb76fd36de09153cf4d8ea49
Joachim Metz <joachim.metz@gmail.com>
2026-06-13 06:26:16 +0200

    Replaced test scripts with autotest

--- tests/msiecf_test_memory.c
+++ tests/msiecf_test_memory.c
@@ -35,15 +35,8 @@
 
 #if defined( HAVE_MSIECF_TEST_MEMORY )
 
-static void *(*msiecf_test_real_malloc)(size_t)                       = NULL;
-static void *(*msiecf_test_real_memcpy)(void *, const void *, size_t) = NULL;
-static void *(*msiecf_test_real_memset)(void *, int, size_t)          = NULL;
-static void *(*msiecf_test_real_realloc)(void *, size_t)              = NULL;
-
-int msiecf_test_malloc_attempts_before_fail                           = -1;
-int msiecf_test_memcpy_attempts_before_fail                           = -1;
-int msiecf_test_memset_attempts_before_fail                           = -1;
-int msiecf_test_realloc_attempts_before_fail                          = -1;
+static void *(*msiecf_test_real_malloc)(size_t) = NULL;
+int msiecf_test_malloc_attempts_before_fail     = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -59,6 +52,11 @@ void *malloc(
 		msiecf_test_real_malloc = dlsym(
 		                           RTLD_NEXT,
 		                           "malloc" );
+
+		if( msiecf_test_real_malloc == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( msiecf_test_malloc_attempts_before_fail == 0 )
 	{
@@ -76,6 +74,9 @@ void *malloc(
 	return( ptr );
 }
 
+static void *(*msiecf_test_real_memcpy)(void *, const void *, size_t) = NULL;
+int msiecf_test_memcpy_attempts_before_fail                           = -1;
+
 /* Custom memcpy for testing memory error cases
  * Note this function might fail if compiled with optimation and as a shared libary
  * Returns a pointer to newly allocated data or NULL
@@ -90,6 +91,11 @@ void *memcpy(
 		msiecf_test_real_memcpy = dlsym(
 		                           RTLD_NEXT,
 		                           "memcpy" );
+
+		if( msiecf_test_real_memcpy == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( msiecf_test_memcpy_attempts_before_fail == 0 )
 	{
@@ -109,6 +115,9 @@ void *memcpy(
 	return( destination );
 }
 
+static void *(*msiecf_test_real_memset)(void *, int, size_t) = NULL;
+int msiecf_test_memset_attempts_before_fail                  = -1;
+
 /* Custom memset for testing memory error cases
  * Note this function might fail if compiled with optimation and as a shared libary
  * Returns a pointer to newly allocated data or NULL
@@ -123,6 +132,11 @@ void *memset(
 		msiecf_test_real_memset = dlsym(
 		                           RTLD_NEXT,
 		                           "memset" );
+
+		if( msiecf_test_real_memset == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( msiecf_test_memset_attempts_before_fail == 0 )
 	{
@@ -142,6 +156,9 @@ void *memset(
 	return( ptr );
 }
 
+static void *(*msiecf_test_real_realloc)(void *, size_t) = NULL;
+int msiecf_test_realloc_attempts_before_fail             = -1;
+
 /* Custom realloc for testing memory error cases
  * Note this function might fail if compiled with optimation
  * Returns a pointer to reallocated data or NULL
@@ -155,6 +172,11 @@ void *realloc(
 		msiecf_test_real_realloc = dlsym(
 		                            RTLD_NEXT,
 		                            "realloc" );
+
+		if( msiecf_test_real_realloc == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( msiecf_test_realloc_attempts_before_fail == 0 )
 	{
