36b34815d80f8b8eb61ac9a2014bff016f361b62
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 13:46:47 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libmsiecf/.libs/libmsiecf.1.dylib && test -f ./pymsiecf/.libs/pymsiecf.so
+if [ -f "${PWD}/libmsiecf/.libs/libmsiecf.1.dylib" ] && [ -f ./pymsiecf/.libs/pymsiecf.so ]
 then
-	install_name_tool -change /usr/local/lib/libmsiecf.1.dylib ${PWD}/libmsiecf/.libs/libmsiecf.1.dylib ./pymsiecf/.libs/pymsiecf.so
+    install_name_tool -change /usr/local/lib/libmsiecf.1.dylib "${PWD}/libmsiecf/.libs/libmsiecf.1.dylib" ./pymsiecf/.libs/pymsiecf.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
